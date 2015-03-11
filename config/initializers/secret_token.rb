cddc6aad5d2facc1952c1a3a2eb585c5c2106355
“wcodes” <wang_mu@hotmail.com>
2015-03-11 00:58:34 +0000

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stukbook::Application.config.secret_key_base = '1479688286e5feac5bc3685ed7513a3aebe39b2c422a108c8873761e8ec3db8cbb739a87575e0666625b0fc4645c96dbf76c06267600c9348e2eeabc62b24f31'
